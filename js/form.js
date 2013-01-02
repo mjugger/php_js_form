af92b6685c57fc899c8b9dfa007d303e7ed0384f
mjugger <xaiox20@gmail.com>
2013-01-02 16:30:28 -0500

    adding changes

---
+++ js/form.js
@@ -0,0 +1,21 @@
+
+
+var projectForm = {
+	globals:{
+		responceJson:null // Holds the json from the server.
+	},
+	
+	initListeners:function(){
+		
+	},
+	
+	callHelper:function(){
+		
+	},
+	
+	switchViews:function(){
+	
+	}
+	
+	
+}
